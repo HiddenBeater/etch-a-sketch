ee8a9b4e8f3305f92ce55c14ec77714a320470cd
LCala Laptop <74427491+HiddenBeater@users.noreply.github.com>
2024-09-17 20:05:54 -0700

    Create Sketch Area in js. start create grid cell function

--- index.js
+++ index.js
@@ -3,3 +3,21 @@ const container = document.getElementsByClassName("container");
 // Add a div
 const div = document.createElement('div');
 container.appendChild(div);
+
+const gridSide = 600;
+let rows = 16;
+let cols = 16;
+
+const sketchArea = document.querySelector('#sketch-area');
+sketchArea.style.width = `${gridSide}`;
+sketchArea.style.height = `${gridSide}`;
+
+function createGridCell(){
+    for (let i = 0; i < (rows * cols); i++){
+            const gridCell = document.createElement('div');
+
+            gridCell.style.width =`(${(gridSide / cols) - 2}px`;
+            gridCell.style.height = `(${(gridSide / rows) - 2}px`;
+            
+    }
+}
