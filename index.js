63a42bac39237ca795e733dcffbf83935842c072
lcalapc <74427491+HiddenBeater@users.noreply.github.com>
2024-09-18 20:52:03 -0700

    Completely redo js only to forget to add px to gridside

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-const gridSide = 600;
+const GRIDSIDE = 600;
 let rows = 16;
 let cols = 16;
 
-const sketchArea = document.querySelector('#sketch-area');
-sketchArea.style.width = `${gridSide}`;
-sketchArea.style.height = `${gridSide}`;
+const sketchArea = document.querySelector("#sketch-area");
+sketchArea.style.width = `${GRIDSIDE}`+ 'px';
+sketchArea.style.heigh = `${GRIDSIDE}` + 'px';
 
-function createGridCell(){
+function createGridCells(){
     for (let i = 0; i < (rows * cols); i++){
-            const gridCell = document.createElement('div');
+        const gridCell = document.createElement('div');
 
-            gridCell.style.width =`(${(gridSide / cols) - 2}px`;
-            gridCell.style.height = `(${(gridSide / rows) - 2}px`;
-            gridCell.classList.add('cell');
+        gridCell.style.width = `${(GRIDSIDE / cols) - 2}px`;
+        gridCell.style.height = `${(GRIDSIDE / rows) - 2}px`;
+        gridCell.classList.add("cell");
 
-            sketchArea.appendChild(gridCell);
+        sketchArea.appendChild(gridCell);
     }
 }
 
-createGridCell();
+createGridCells();
